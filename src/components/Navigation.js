ad7a4fe9c72952d6bf07497f5c08198d2297506c
Joonas <asd@asd.fi>
2021-04-21 17:10:57 +0300

    RouterLinks placed in navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,22 +1,27 @@
+/* eslint-disable max-len */
 import {Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
 import {Search} from 'react-bootstrap-icons';
+import {Link as RouterLink} from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <>
       <Navbar>
-        <Navbar.Brand href="#home"
+        <Navbar.Brand
           style={{
             paddingLeft: '1.25rem',
           }}
         >
-          <img
-            src="logo512.png"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-            alt="React Bootstrap logo"
-          />
+          <RouterLink to="/home">
+            <img
+              src="logo512.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </RouterLink>
         </Navbar.Brand>
         <Form inline className="mr-0 position-absolute"
           style={{
@@ -41,11 +46,11 @@ const Navigation = () => {
           </Button>
         </Form>
         <Nav className="justify-content-end w-100">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#upload">Upload</Nav.Link>
-          <Nav.Link href="#profile">Profile</Nav.Link>
-          <Nav.Link href="#favourites">Favourites</Nav.Link>
-          <Nav.Link href="#logout">Logout</Nav.Link>
+          <Nav.Link href="#home"><RouterLink to="/home">Home</RouterLink></Nav.Link>
+          <Nav.Link href="#upload"><RouterLink to="/upload">Upload</RouterLink></Nav.Link>
+          <Nav.Link href="#profile"><RouterLink to="/profile">Profile</RouterLink></Nav.Link>
+          <Nav.Link href="#favourites"><RouterLink to="/favourites">Favourites</RouterLink></Nav.Link>
+          <Nav.Link href="#logout"><RouterLink to="/logout">Logout</RouterLink></Nav.Link>
         </Nav>
       </Navbar>
     </>
@@ -53,5 +58,5 @@ const Navigation = () => {
 };
 
 
-export default Navigation;
+export default withRouter(Navigation);
 
