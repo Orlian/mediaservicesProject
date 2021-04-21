959d5610aafc2d0b2465c13543435dceb62e07f1
Joonas <asd@asd.fi>
2021-04-21 16:59:57 +0300

    Home-view updating

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,8 +1,27 @@
+/* eslint-disable max-len */
+
 const Home = () => {
   return (
-    <div className={'row'}>
-      <div className={'col-12 banner'}>test</div>
-    </div>
+    <>
+      <div className={'row-cols'}>
+        <div className={'col-12 banner'}>This is banner lol</div>
+      </div>
+      <div className={'row-cols justify-content-center mt-3'}>
+        <div className={'col-6'}>
+          <ul
+            className={'d-flex home-sort-ul justify-content-between list-unstyled'}>
+            <li className={'home-sort-li'}><a className="nav-link"
+              href="#asd1">asd1</a></li>
+            <li className={'home-sort-li'}><a className="nav-link"
+              href="#asd2">asd2</a></li>
+            <li className={'home-sort-li'}><a className="nav-link"
+              href="#asd3">asd3</a></li>
+            <li className={'home-sort-li'}><a className="nav-link"
+              href="#asd4">asd4</a></li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 
