93b914e4d8f2466bef4328002ab6645331cbe30d
Joonas <asd@asd.fi>
2021-04-21 13:54:33 +0300

    Added routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navigation from './components/Navigation';
+import Login from './view/Login';
+import Home from './view/Home';
+import Edit from './view/Edit';
+import Profile from './view/Profile';
+import Single from './view/Single';
+import Upload from './view/Upload';
+import Logout from './view/Logout';
 
 const App = () => {
   return (
@@ -7,7 +14,13 @@ const App = () => {
       <div className="container-fluid">
         <Navigation/>
         <Switch>
-          <Route />
+          <Route path="/" component={Login} />
+          <Route path="/home" component={Home} />
+          <Route path="/edit" component={Edit} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/single" component={Single} />
+          <Route path="/upload" component={Upload} />
+          <Route path="/logout" component={Logout} />
         </Switch>
       </div>
     </Router>
