1c9577faafb739520b311f2d8b9a8edab7576512
irina <irina.konovalova455@gmail.com>
2021-04-21 13:18:01 +0300

    just testing components

---
+++ src/components/RegisterForm.js
@@ -0,0 +1,35 @@
+import {Form, Button} from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+const RegisterForm = () => {
+  return (
+
+    <>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            Well never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
+
+  );
+};
+RegisterForm.propTypes = {
+  setToggle: PropTypes.func,
+};
+export default RegisterForm;
