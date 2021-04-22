72fba5b93d0d62ee42334a88d9c4b7945f1fb5f9
Joonas <asd@asd.fi>
2021-04-22 11:59:33 +0300

    MediaRow and MediaTable improvement

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,4 +1,5 @@
 /* eslint-disable max-len */
+import MediaTable from '../components/MediaTable';
 
 const Home = () => {
   return (
@@ -8,17 +9,7 @@ const Home = () => {
       </div>
       <div className={'row-cols d-flex justify-content-center mt-3'}>
         <div className={'col-6'}>
-          <ul
-            className={'d-flex home-sort-ul justify-content-between list-unstyled'}>
-            <li className={'home-sort-li'}><a className="nav-link"
-              href="#asd1">asd1</a></li>
-            <li className={'home-sort-li'}><a className="nav-link"
-              href="#asd2">asd2</a></li>
-            <li className={'home-sort-li'}><a className="nav-link"
-              href="#asd3">asd3</a></li>
-            <li className={'home-sort-li'}><a className="nav-link"
-              href="#asd4">asd4</a></li>
-          </ul>
+          <MediaTable />
         </div>
       </div>
     </>
