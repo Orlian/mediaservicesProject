758690032b3924546c4e25508f4a959b3ace6139
35840 <osku1993@luukku.com>
2021-04-27 10:28:54 +0300

    upload toimii

--- src/view/Upload.js
+++ src/view/Upload.js
@@ -1,8 +1,8 @@
 import {Button, Form, Spinner} from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import {useEffect} from 'react';
+import {useState, useEffect} from 'react';
 import CancelButton from '../components/CancelButton';
-import {useFormik} from 'formik';
+import {Formik, Field} from 'formik';
 import {useMedia, useTag} from '../hooks/ApiHooks';
 import * as Yup from 'yup';
 
@@ -11,29 +11,53 @@ const Upload = ({history}) => {
   const {postMedia, loading} = useMedia();
   const {postTag} = useTag();
 
-  const formik = useFormik({
-    initialValues: {
-      file: null,
-      title: '',
-      description: '',
-      checked: [],
-    },
-    validationSchema: Yup.object({
-      file: Yup.mixed()
-          .required('Required'),
-      title: Yup.string()
-          .max(20, 'Must be 20 characters or less')
-          .required('Required'),
-      description: Yup.string().required('Required'),
-    }),
-    onSubmit: (values) => {
-      doUpload(values);
-      alert(JSON.stringify(values, null, 2));
-    },
+  const supportedFormats = [
+    'image/jpg',
+    'image/jpeg',
+    'image/gif',
+    'image/png',
+    'image/heic',
+    'image/heif',
+  ];
+
+  const [file, setFile] = useState({file: null, dataUrl: ''});
+
+  const onChange = (evt) => {
+    if (supportedFormats.includes(evt.currentTarget.files[0].type)) {
+      setFile({file: evt.currentTarget.files[0]});
+    } else {
+      setFile({file: null, dataUrl: ''});
+    }
+  };
+
+  const initialValues = {
+    file: null,
+    title: '',
+    description: '',
+    checked: [],
+  };
+
+
+  const validationSchema = Yup.object({
+    file: Yup.mixed()
+        .required('Required').test(
+            'fileFormat',
+            'Unsupported Format',
+            (value) =>{
+              console.log('validation file', file.file);
+              return file.file ? true : false;
+            },
+        ),
+    title: Yup.string()
+        .max(20, 'Must be 20 characters or less')
+        .required('Required'),
+    description: Yup.string().required('Required'),
   });
+
 
   const doUpload = async (inputs) => {
     try {
+      console.log('blääh', inputs.file);
       const fd = new FormData();
       fd.append('title', inputs.title);
       // kuvaus + filtterit tallennetaan description kenttään
@@ -42,7 +66,7 @@ const Upload = ({history}) => {
         genres: inputs.checked,
       };
       fd.append('description', JSON.stringify(desc));
-      fd.append('file', inputs.file);
+      fd.append('file', file.file);
       const result = await postMedia(fd, localStorage.getItem('token'));
       const tagResult = await postTag(localStorage.getItem('token'),
           result.file_id);
@@ -57,144 +81,160 @@ const Upload = ({history}) => {
   useEffect(() => {
     const reader = new FileReader();
 
-    reader.addEventListener('load', () => {
-      setInputs((inputs) => ({
-        ...inputs,
+    const setImage = () => {
+      setFile((file) => ({
+        ...file,
         dataUrl: reader.result,
       }));
-    });
+    };
 
-    if (inputs.file !== null) {
-      if (inputs.file.type.includes('image')) {
-        reader.readAsDataURL(inputs.file);
+    reader.addEventListener('load', setImage);
+
+    if (file.file !== null) {
+      if (file.file.type.includes('image')) {
+        reader.readAsDataURL(file.file);
       } else {
-        setInputs((inputs) => ({
-          ...inputs,
+        setFile((file) => ({
+          ...file,
           dataUrl: 'logo512.png',
         }));
       }
     }
-  }, [inputs.file]);
+
+    return () => {
+      reader.removeEventListener('load', setImage);
+    };
+  }, [file.file]);
+
+  console.log('inputs', file);
 
   return (
     <>
       <div className="row-cols d-flex justify-content-center">
         <div className="col-lg-3 mt-5" >
           {!loading ?
-            <Form
-              onSubmit={formik.handleSubmit}
-              className="p-4 rounded-lg"
-              style={{backgroundColor: '#f8f8ff'}}
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={(values, {setSubmitting, resetForm}) => {
+                setSubmitting(true);
+                doUpload(values);
+                setTimeout(() => {
+                  resetForm();
+                  setSubmitting(false);
+                }, 500);
+              }}
             >
-              <h1>Upload Content</h1>
-              <Form.Group controlId="form-title">
-                <Form.Label>File</Form.Label>
-                <Form.Control
-                  id="form-file"
-                  name="file"
-                  type="file"
-                  accept="image/*, audio/*, video/*"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.file}
-                />
-                {formik.touched.file && formik.errors.file ? (
-                  <div>{formik.errors.file}</div>
-                ) : null}
-              </Form.Group>
-              <Form.Group controlId="form-title">
-                <Form.Label>Title</Form.Label>
-                <Form.Control type="text" placeholder="Title" name="title"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.title}
-                />
-                {formik.touched.title && formik.errors.title ? (
-                  <div>{formik.errors.title}</div>
-                ) : null}
-              </Form.Group>
-              <Form.Group controlId="form-desc">
-                <Form.Label>Description</Form.Label>
-                <Form.Control type="text"
-                  placeholder="Description"
-                  name="desc"
-                  as="textarea"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.description}
-                  style={{
-                    resize: 'none',
-                  }}
-                />
-                {formik.touched.description && formik.errors.description ? (
-                  <div>{formik.errors.description}</div>
-                ) : null}
-              </Form.Group>
-              <Form.Group>
-                <h2>Choose genres</h2>
-                {['checkbox'].map((type) => (
-                  <div key={`inline-${type}`} className="mb-3">
-                    <Form.Check inline label="EDM"
-                      type={type} id={`inline-${type}-1`}
-                      name="checked"
-                      value="EDM"
+              {( {values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                setFieldValue,
+                isSubmitting}) => (
+                <Form
+                  onSubmit={handleSubmit}
+                  className="p-4 rounded-lg"
+                  style={{backgroundColor: '#f8f8ff'}}
+                >
+                  <h1>Upload Content</h1>
+                  <Form.Group>
+                    <Form.Label>File</Form.Label>
+                    <img src={file.dataUrl}/>
+                    <Form.Control
+                      id="form-file"
+                      name="file"
+                      type="file"
+                      onChange={(evt)=>{
+                        handleChange(evt);
+                        onChange(evt);
+                      }}
+                      onBlur={handleBlur}
+                      setFieldValue={setFieldValue}
+                      className={touched.file && errors.file ?
+                        'error' : null}
                     />
-                    <Form.Check inline label="Hip-hop/ Rap"
-                      type={type} id={`inline-${type}-2`}
-                      name="checked"
-                      value="Hip-hop/ Rap"
+                    {touched.file && errors.file ? (
+                      <div className="error-message">{errors.file}</div>
+                    ): null}
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control type="text" placeholder="Title" name="title"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.title}
                     />
-                    <Form.Check
-                      inline
-                      label="Rock"
-                      type={type}
-                      id={`inline-${type}-3`}
-                      name="checked"
-                      value="Rock"
+                    {touched.title && errors.title ? (
+                      <div className="error-message">{errors.title}</div>
+                    ): null}
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control type="text"
+                      placeholder="Description"
+                      name="description"
+                      as="textarea"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.description}
+                      style={{
+                        resize: 'none',
+                      }}
                     />
-                    <Form.Check
-                      inline
-                      label="Pop"
-                      type={type}
-                      id={`inline-${type}-4`}
-                      name="checked"
-                      value="Pop"
-                    />
-                    <Form.Check
-                      inline
-                      label="Metal"
-                      type={type}
-                      id={`inline-${type}-5`}
-                      name="checked"
-                      value="Metal"
-                    />
-                    <Form.Check
-                      inline
-                      label="Alternative"
-                      type={type}
-                      id={`inline-${type}-6`}
-                      name="checked"
-                      value="Alternative"
-                    />
-                  </div>
-                ))}
-              </Form.Group>
-              <Form.Group className="d-flex justify-content-center">
-                <Button type="submit"
-                  className="w-50 font-weight-bold form-btn"
-                  style={{
-                    backgroundColor: '#f6aa1c',
-                    border: '1px solid #f6aa1c',
-                    color: '#161616',
-                    borderRadius: '30em',
-                  }}>
+                    {touched.description && errors.description ? (
+                      <div className="error-message">{errors.description}</div>
+                    ): null}
+                  </Form.Group>
+                  <Form.Group>
+                    <h2>Choose genres</h2>
+                    <div role="group" aria-labelledby="checkbox-group">
+                      <label>
+                        <Field type="checkbox" name="checked" value="EDM" />
+                        EDM
+                      </label>
+                      <label>
+                        <Field type="checkbox" name="checked"
+                          value="Hip-hop/ Rap" />
+                        Hip-hop/ Rap
+                      </label>
+                      <label>
+                        <Field type="checkbox" name="checked" value="Rock" />
+                        Rock
+                      </label>
+                      <label>
+                        <Field type="checkbox" name="checked" value="Pop" />
+                        Pop
+                      </label>
+                      <label>
+                        <Field type="checkbox" name="checked" value="Metal" />
+                        Metal
+                      </label>
+                      <label>
+                        <Field type="checkbox" name="checked"
+                          value="Alternative" />
+                        Alternative
+                      </label>
+                    </div>
+                  </Form.Group>
+                  <Form.Group className="d-flex justify-content-center">
+                    <Button type="submit"
+                      className="w-50 font-weight-bold form-btn"
+                      style={{
+                        backgroundColor: '#f6aa1c',
+                        border: '1px solid #f6aa1c',
+                        color: '#161616',
+                        borderRadius: '30em',
+                      }}>
                   UPLOAD
-                </Button>
-              </Form.Group>
-              <Form.Group className="d-flex justify-content-center">
-                <CancelButton/>
-              </Form.Group>
-            </Form> :
+                    </Button>
+                  </Form.Group>
+                  <Form.Group className="d-flex justify-content-center">
+                    <CancelButton/>
+                  </Form.Group>
+                </Form> )}
+            </Formik>:
             <Spinner animation="border" />
           }
         </div>
