6c85ec0f9c43f1c9202bdd9fa417c6237a44ff38
35840 <osku1993@luukku.com>
2021-05-02 18:23:22 +0300

    indentifier muutos

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,5 +1,5 @@
 const baseUrl = 'https://media-new.mw.metropolia.fi/wbma/';
 const uploadsUrl = 'https://media-new.mw.metropolia.fi/wbma/uploads/';
-const appIdentifier = 'hurdygurdy';
+const appIdentifier = 'hurdygurdy1';
 
 export {baseUrl, uploadsUrl, appIdentifier};
