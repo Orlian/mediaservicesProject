67a9cf218f780200ec9cdd29208ddc5efebaf42b
Joonas <asd@asd.fi>
2021-05-04 17:47:24 +0300

    merge

--- src/view/Search.js
+++ src/view/Search.js
@@ -1,35 +1,42 @@
-import {Col, Container, Row} from 'react-bootstrap';
+import {Col, Container, Row, Spinner} from 'react-bootstrap';
 import UserTable from '../components/UserTable';
 import {useContext} from 'react';
 import {MediaContext} from '../contexts/MediaContext';
 import PropTypes from 'prop-types';
+import {useUsers} from '../hooks/ApiHooks';
 
 
 const Search = ({location}) => {
   const [user] = useContext(MediaContext);
+  const {loading} = useUsers();
   const input = location.state;
 
   console.log(input);
 
   return (
     <>
-      <Container fluid className="bg-dark"
-        style={{
-          minHeight: '100vh',
-        }}>
-        <Row className="d-flex justify-content-center pt-5">
-          <Col xs={'auto'}>
-            <h1 style={{
-              color: '#f8f8ff',
-            }}>Search results for {input}</h1>
-          </Col>
+      {!loading ?
+        <Container fluid className="bg-dark"
+          style={{
+            minHeight: '100vh',
+          }}>
+          <Row className="d-flex justify-content-center pt-5">
+            <Col xs={'auto'}>
+              <h1 style={{
+                color: '#f8f8ff',
+              }}>Search results for {input}</h1>
+            </Col>
+          </Row>
+          <Row className="d-flex justify-content-center mt-5">
+            <Col xs={10}>
+              <UserTable user={user} input={input} sortType={'all'}/>
+            </Col>
+          </Row>
+        </Container> :
+        <Row className="d-flex justify-content-center">
+          <Spinner animation={'border'} />
         </Row>
-        <Row className="d-flex justify-content-center mt-5">
-          <Col xs={10}>
-            <UserTable user={user} input={input} sortType={'all'}/>
-          </Col>
-        </Row>
-      </Container>
+      }
     </>
   );
 };
