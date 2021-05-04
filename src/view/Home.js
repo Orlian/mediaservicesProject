dc0361e78a6f6387bf07c1aacca521c33ed6d96d
irina <irina.konovalova455@gmail.com>
2021-05-05 00:12:11 +0300

    home view about

--- src/view/Home.js
+++ src/view/Home.js
@@ -61,15 +61,15 @@ const Home = () => {
               <div className="p-3"
                 style={{background: 'rgba(0, 0, 0, 0.4)'}}>
                 <h1>Musikantti</h1>
-                <h2>Collaboration platform for musicians</h2>
+                <h2>Bringing music creators together</h2>
               </div>
-              <div>
+              <div className="d-flex flex-column justify-content-between">
                 {user ?
                   <Button
                     className="mt-5 banner-btn px-5 py-2"
                     href="#home-cards-area"
                   >DISCOVER
-                  </Button> : <Button
+                  </Button> : <> <Button
                     className="mt-5 banner-btn px-5 py-2"
                     as={Link} to={
                       {
@@ -77,7 +77,10 @@ const Home = () => {
                       }
                     }>
                     REGISTER
-                  </Button>}
+                  </Button>
+                  <Button href="#about" className="mt-3 about-button">ABOUT</Button>
+                  </>
+                }
               </div>
             </div>
           </Col>
@@ -194,37 +197,38 @@ const Home = () => {
                 <h2 className="text-center p-5" id="about">About Musikantti</h2>
               </Col>
             </Row>
-            <Container style={{
+            <Container id="about-container" style={{
               backgroundColor: 'rgba(0, 0, 0, 0.2)',
             }}>
               <Row>
-                <Col xs={6} className="d-flex justify-content-end p-0">
-                  <Image src="placeholder.jpg" className="w-100"/>
-                </Col>
-                <Col xs={6} className="p-0">
+                <Col sm={{span: 12, order: 1}} md={{span: 6, order: 1}} className="p-0">
                   <div>
                     <h3 className="text-center p-3">What is Musikantti</h3>
                     <p className="mx-5">Musikantti is all about bringing music
-                    creators together. If you enjoy creating music-related
+                      creators together. If you enjoy creating music-related
                       content, such as  playing, composing or producing music,
                       Musikantti will help you find like-minded people to
                       collaborate with.  </p>
                   </div>
                 </Col>
+                <Col sm={{span: 12, order: 2}} md={{span: 6, order: 2}} className="d-flex justify-content-end p-0">
+                  <Image src="placeholder.jpg" className="w-100"/>
+                </Col>
               </Row>
               <Row>
-                <Col xs={6} className="p-0">
+                <Col sm={{span: 12, order: 4}} md={{span: 6, order: 3}} className="p-0">
+                  <Image src="placeholder-2.jpg" className="w-100"/>
+                </Col>
+                <Col sm={{span: 12, order: 3}} md={{span: 6, order: 4}} className="p-0">
                   <div>
                     <h3 className="text-center p-3">What you can do in Musikantti</h3>
-                    <ul className="mx-5">
-                      <li>Create a portfolio, where you can display your music, videos and photography or illustrations</li>
-                      <li>Find like-minded creators recommended to you based on your region, musical skills and genre preferences </li>
+                    <ul className="mx-5 list-unstyled">
+                      <li className="mb-3">Create a portfolio, where you can display your music, videos and photography or illustrations.</li>
+                      <li className="mb-3">Find like-minded creators recommended to you based on your region, musical skills and genre preferences. </li>
                     </ul>
                   </div>
                 </Col>
-                <Col xs={6} className="p-0">
-                  <Image src="placeholder-2.jpg" className="w-100"/>
-                </Col>
+
               </Row>
             </Container>
           </>
