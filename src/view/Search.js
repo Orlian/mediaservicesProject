011179c907f2df85f57aae3f398806593877c714
Joonas <asd@asd.fi>
2021-05-05 18:55:23 +0300

    search for yourself bug is fixed

--- src/view/Search.js
+++ src/view/Search.js
@@ -1,43 +1,35 @@
-import {Col, Container, Row, Spinner} from 'react-bootstrap';
+import {Col, Container, Row} from 'react-bootstrap';
 import UserTable from '../components/UserTable';
 import {useContext} from 'react';
 import {MediaContext} from '../contexts/MediaContext';
 import PropTypes from 'prop-types';
-import {useUsers} from '../hooks/ApiHooks';
-
 
 const Search = ({location}) => {
   const [user] = useContext(MediaContext);
-  const {loading} = useUsers();
   const input = location.state;
 
   console.log(input);
 
   return (
     <>
-      {!loading ?
-        <Container fluid className="bg-dark"
-          style={{
-            minHeight: '100vh',
-          }}>
-          <Row className="d-flex justify-content-center pt-5">
-            <Col xs={'auto'}>
-              <h1 className="h3" style={{
-                color: '#f8f8ff',
-                textAlign: 'center',
-              }}>Search results for {input}</h1>
-            </Col>
-          </Row>
-          <Row className="d-flex justify-content-center mt-5">
-            <Col xs={10}>
-              <UserTable user={user} input={input} sortType={'all'}/>
-            </Col>
-          </Row>
-        </Container> :
-        <Row className="d-flex justify-content-center">
-          <Spinner animation={'border'} />
+      <Container fluid className="bg-dark"
+        style={{
+          minHeight: '100vh',
+        }}>
+        <Row className="d-flex justify-content-center pt-5">
+          <Col xs={'auto'}>
+            <h1 className="h3" style={{
+              color: '#f8f8ff',
+              textAlign: 'center',
+            }}>Search results for {input}</h1>
+          </Col>
         </Row>
-      }
+        <Row className="d-flex justify-content-center mt-5">
+          <Col xs={10}>
+            <UserTable user={user} input={input} sortType={'all'}/>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
